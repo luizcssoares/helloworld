d31178736ec1f7efa25c08a3effe716f2919b0b2
Luiz Carlos Soares <luiz.soares@pdcase.com.br>
2026-07-02 14:11:30 -0300

    Commit da branch ajuste1

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -3,11 +3,14 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+ Comentario da branch AJUSTE1
+*/
 @Controller
 public class HelloWorldController {
     @GetMapping("/")
     public String helloworld(){
-        return "hello world";
+        return "hello world ajuste1";
     }
 
 }
